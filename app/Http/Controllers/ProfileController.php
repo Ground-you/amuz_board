ee20aa99a5846a867b167b342e39c0e20c4a3c32
Ground-you <ongroundyou@gmail.com>
2026-05-29 10:45:27 +0900

    계정삭제 오류 수정

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Inertia\Inertia;
 
 class ProfileController extends Controller
